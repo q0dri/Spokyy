fa6c46a7fcb865154800bb970a0b3c4af9ad4382
q0dri <52640563+q0dri@users.noreply.github.com>
2023-04-09 04:05:03 +0700

    Initial commit

---
+++ contracts/SpookyTest.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract SpookyTest is ERC20, ERC20Burnable, ERC20Permit {
+    constructor() ERC20("SpookyTest", "") ERC20Permit("SpookyTest") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+}
